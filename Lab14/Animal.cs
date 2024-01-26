d8d2df0dde8419bf741e90c7b34efc39da28d3ca
Артём Белоусов <belousov@psksk.ru>
2024-01-26 13:44:45 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; }
+
+        public Animal()
+        {
+            
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название: {Name}");
+            Say();
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override string Name => "Кошка";
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        public override string Name => "Собака";
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
---
+++ Lab14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat();
+            cat.ShowInfo();
+
+            Console.WriteLine();
+
+            Animal dog = new Dog();
+            dog.ShowInfo();
+        }
+    }
+}
+
